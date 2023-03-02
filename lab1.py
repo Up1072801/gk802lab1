2bb8f8a63904fb373f53687e58214b7d35415fa5
nikos 7 <up1072801@upnet.gr>
2023-03-02 19:53:22 +0200

    Deutera erotimata

--- lab1.py
+++ lab1.py
@@ -1,10 +1,26 @@
 import requests  # εισαγωγή της βιβλιοθήκης
-from datetime import datetime
-x = input("Give URL: " )
+import requests
+from datetime import datetime;
+x = input("Enter URL: ")
 r = requests.get(x)
-for i in r.headers:
-    print(i);
-
+print("\nHeaders: \n")
+for i in r.headers :
+    print (i)
+print("\nServer: " + r.headers['Server'])
+if('Set-Cookie'in r.headers):
+    print("\nThe URL Cookies: \n")
+    for j in r.cookies:
+        print(j.name)
+        if j.expires==None:
+            print("Does Not Expire\n")
+        else:
+            e=float(j.expires)
+            n=datetime.now().timestamp()
+            print(" Expires In: ")
+            print(datetime.fromtimestamp(e)-datetime.fromtimestamp(d))
+            print("\n")
+else:
+    print("No Cookie")
 
 
 
